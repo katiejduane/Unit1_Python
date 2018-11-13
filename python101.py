85fa0bead1e11e0042e46874760ba6fbd377c3b0
Katie Duane <katiejduane@gmail.com>
2018-11-13 12:02:48 -0500

    First commit of python101.py for class

---
+++ python101.py
@@ -0,0 +1,27 @@
+print "Hello World"
+print """
+It was a dark and stormy night.
+It was a good second line.
+"""
+
+# Variables: strings, letters, numbers, or any other stuff you can make with a keyboard!
+# Variables do not make the program faster; they make it slower. 
+# Variables make it easier for us (developers) to wrtie programs.
+
+theBestClass = "our class"
+the_best_class = "our class"
+
+print theBestClass
+
+# "\n" = new line!
+
+# Data Types: Programming languages see different types of variables differently. 
+# - Strings: English stuff
+# - Numbers: integers/floats
+# - Booleans: True or False, on or off, 1 or 0
+# - Lists: list of things in one variable
+# - Dictionaries: variable of variables
+# - Objects: Super Dictionaries!
+
+# Primitive Data Types; Strings, numbers ,booleans
+# Abstract Data Types: Lists, Dictionaries, Objects
