709f5f735a3c58674338a1794c41030f89638434
Katie Duane <katiejduane@gmail.com>
2018-11-14 22:03:18 -0500

    for loops exercises done for homework

--- for_loops_exercises.py
+++ for_loops_exercises.py
@@ -1,15 +1,42 @@
 # LOOPS EXERCISES!
 
 # 1-10
-for i in range(1, 11):
-    print i
+# for i in range(1, 11):
+#     print i
 
-# N to M
-start = int(raw_input("Type a number to start: "))
-end = int(raw_input("Type a number to end: "))
-for i in range(start, end):
-    print i
+# # N to M
+# start = int(raw_input("Type a number to start: "))
+# end = int(raw_input("Type a number to end: "))
+# for i in range(start, end):
+#     print i
 
 # Odd Numberz
-for i in range (1, 10, 2):
-    print i
+# for i in range (1, 10, 2):
+#     print i
+
+# # Print a square!
+# length = 5 * " * "
+# for i in range(1, 6):
+#     print length
+
+# #Print a box shape!
+# dimension = int(raw_input("How many inches wide is the box? "))
+# width = dimension * " * "
+# for i in range (1, dimension + 1):
+#     print width
+
+#Hollow Box
+box_width = int(raw_input("What's the box's width? "))
+box_height = int(raw_input("What's the box's height? "))
+space = " "
+
+for i in range(box_height):
+    if (i == 0 or i == box_height - 1):
+        print box_width * "*"
+    else:
+        print '*' + space * (box_width - 2) + "*"
+
+# Triangle 1
+
+
+
