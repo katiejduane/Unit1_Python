ac8d470e93de5f2f9fa0f78b9c92c3d58ec6dfc9
Katie Duane <katiejduane@gmail.com>
2018-11-14 19:22:13 -0500

    For loops exercises beginning

---
+++ for_loops_exercises.py
@@ -0,0 +1,15 @@
+# LOOPS EXERCISES!
+
+# 1-10
+for i in range(1, 11):
+    print i
+
+# N to M
+start = int(raw_input("Type a number to start: "))
+end = int(raw_input("Type a number to end: "))
+for i in range(start, end):
+    print i
+
+# Odd Numberz
+for i in range (1, 10, 2):
+    print i
